2cf58ffe8c9333c4f29ae2989320f7183553eac1
Christian Johansen <christian@cjohansen.no>
2014-08-20 15:09:28 +0200

    Remove unneeded partials

--- src/prone/hiccough.clj
+++ src/prone/hiccough.clj
@@ -12,20 +12,6 @@
        (str/join (flatten contents))
        "</" (name type) ">"))
 
-(def p (partial tag :p))
-(def h1 (partial tag :h1))
-(def h2 (partial tag :h2))
-(def h3 (partial tag :h3))
-(def div (partial tag :div))
-(def span (partial tag :span))
-(def strong (partial tag :strong))
-(def header (partial tag :header))
-(def section (partial tag :section))
-(def nav (partial tag :nav))
-(def ul (partial tag :ul))
-(def li (partial tag :li))
-(def a (partial tag :a))
-
 (defn- render-1 [form]
   (cond
    (nil? form) ""
