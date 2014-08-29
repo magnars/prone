817b40a27c1132bf03b3b0e86b76827e2a0efee1
Christian Johansen <christian@cjohansen.no>
2014-08-29 15:09:27 +0200

    0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prone "0.4.0-SNAPSHOT"
+(defproject prone "0.3.2"
   :description "Better exception reporting middleware for Ring."
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-shell "0.3.0"]]
