e78b0fcfb9bcbf8f4d9c01a69e2fe02159de57f4
Magnar Sveen <magnars@gmail.com>
2014-12-15 18:19:09 +0100

    Release 0.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prone "0.6.1"
+(defproject prone "0.7.0"
   :description "Better exception reporting middleware for Ring."
   :url "http://github.com/magnars/prone"
   :license {:name "GNU General Public License v3"
