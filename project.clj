d2f07b7b63dd7fad244cc7c49f504d0069762d4a
Magnar Sveen <magnars@gmail.com>
2018-04-04 14:19:17 +0200

    Release 1.5.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prone "1.5.0"
+(defproject prone "1.5.1"
   :description "Better exception reporting middleware for Ring."
   :url "http://github.com/magnars/prone"
   :license {:name "BSD-3-Clause"
