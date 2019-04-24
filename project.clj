ae628671abde70dc518680b5db051f2e4d7a2263
Magnar Sveen <magnars@gmail.com>
2019-04-24 13:20:39 +0200

    Bump realize: now guards against infinite lazy seqs

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/magnars/prone"
   :license {:name "BSD-3-Clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[realize "1.1.0"]]
+  :dependencies [[realize "2019-04-24"]]
   :filespecs [{:type :fn
                :fn (fn [_]
                      {:type :bytes :path "prone/generated/prone.js"
